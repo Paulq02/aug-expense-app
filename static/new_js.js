a5c21575351561891fdeeba716c1b64edd31fe42
paulq02 <paulq10@gmail.com>
2024-12-02 07:32:44 -0800

    sidebar hover features working

--- static/new_js.js
+++ static/new_js.js
@@ -2,9 +2,11 @@ const openSidebar = document.querySelector(".close");
 const toggleButton = document.querySelector(".toggle");
 const toggleSwitch = document.querySelector(".toggle-switch");
 const bodyLight = document.querySelector("body");
+const closeSearchBox = document.querySelector(".open-search-box");
 
 toggleButton.addEventListener("click", () => {
   openSidebar.classList.toggle("sidebar");
+  closeSearchBox.classList.toggle("close-search-box");
 });
 
 toggleSwitch.addEventListener("click", () => {
